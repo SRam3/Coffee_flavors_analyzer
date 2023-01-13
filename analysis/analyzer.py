c7b3c7212ae71fa6309d2e30326880ba159e2d72
Sebastian Ramirez <seramirezra@unal.edu.co>
2023-01-13 18:40:29 -0500

    Begin of qualitative data extraction

--- analysis/analyzer.py
+++ analysis/analyzer.py
@@ -1,17 +1,21 @@
 import sys
 
-from util import Coffee
+from util import Exploration, Coffee
 
 def main():
     if len(sys.argv) > 2:
         sys.exit("Usage: python analizer.py <directory>")
     directory = sys.argv[1] if len(sys.argv) == 2 else "data.json"
     try:
+        data_exploration = Exploration(directory)
         coffee = Coffee(directory)
     except FileNotFoundError:
         sys.exit("File not found")
 
-    coffee.get_correlation()
+    # data_exploration.get_histogram()
+    # data_exploration.get_description()
+    coffee.qualitative_sensorial_attributes()
+
 
 if __name__ == "__main__":
     main()
--- analysis/util.py
+++ analysis/util.py
@@ -1,10 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-class Coffee:
-    """
-    Coffee class
-    """
+
+class Exploration:
     def __init__(self, directory: str):
         """
         Load data from directory
@@ -25,21 +23,51 @@ class Coffee:
     def get_histogram(self):
         """
         Get histogram of the values distribution
-        :param data: pandas dataframe
-        :param column: column name
         :return: None
         """
-        self.data.drop('ID', axis=1, inplace=True)
-        self.data.sort_values('Fecha', inplace=True)
+        self.data.drop("ID", axis=1, inplace=True)
+        self.data.sort_values("Fecha", inplace=True)
         self.data.hist(bins=10, figsize=(20, 15))
         plt.show()
-        plt.savefig('histogram.png')
+        plt.savefig("histogram.png")
 
-    def get_correlation(self):
+    def get_description(self):
         """
-        Get correlation matrix
-        :param data: pandas dataframe
+        Get data description
         :return: None
         """
-        corr_matrix = self.data.corr()
-        corr_matrix.to_csv('correlation.csv')
+        print(self.data.describe())
+
+
+class Coffee(Exploration):
+    def __init__(self, directory: str):
+        super().__init__(directory)
+
+    def physical_atributes(self):
+        """
+        Get physical attributes of coffee
+        :return: None
+        """
+        phys_atributes = self.data[["Almendra sana", "Broca", "Humedad", "Pasilla"]]
+        print(phys_atributes.describe())
+
+    def quantitative_sensorial_attributes(self):
+        """
+        Get quantitative sensorial attributes of coffee
+        :return: None
+        """
+        quant_sensorial_atributes = self.data[
+            ["Acidez", "Balance", "Cuerpo", "Dulzura", "Fragancia/Aroma", "Sabor", "Sabor Residual"]
+        ]
+        print(quant_sensorial_atributes.describe())
+
+    def qualitative_sensorial_attributes(self):
+        """
+        Get qualitative sensorial attributes of coffee
+        :return: None
+        """
+        new_data = self.data.copy()
+        for i, l in enumerate(new_data['Descripcion']):
+            new_data['Descripcion'][i] = l.split(',')
+            new_data = new_data.explode('Descripcion')
+        print(new_data['Descripcion'].iloc[0:10])
